3e854e06fa4be74dc2ba9e399535034f52bad0e1
Dean Puttera <128890125+Deanramadhan08@users.noreply.github.com>
2023-05-10 08:22:01 +0700

    Add files via upload

---
+++ node-js-moduleBiodata.js
@@ -0,0 +1,11 @@
+const biodata = {
+  nama: "Dean Ramadhan",
+  tempatLahir: "Jakarta",
+  tanggalLahir: "08 Maret 1994",
+  alamat: "Jl. Wicaya No. 22",
+};
+
+console.log("Nama :" + biodata.nama);
+console.log("Tempat Lahir :" + biodata.tempatLahir);
+console.log("Tanggal Lahir :" + biodata.tanggalLahir);
+console.log("Alamat :" + biodata.alamat);
